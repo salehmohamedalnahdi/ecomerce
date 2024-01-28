939c9902a47b147454d22ee3f57b2071e754f641
saleh <you@example.com>
2024-01-28 20:04:16 +0300

    delete

--- p/account/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-#from import_export.admin import ImportExportModelAdmin
-admin.site.register(Profile)
-#@admin.site.register(Profile)
-#class ProfileImportExport(ImportExportModelAdmin):
-   # pass
-# Register your models here.
